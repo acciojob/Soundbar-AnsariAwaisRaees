6aa1759087e2db30e62a939fda6ff22aa73d841c
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-12-01 18:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function play(){
+	var audio = document.getElementById("audio");
+	audio.play();
+}
+
+function stop() {
+	var audio = docemnt.getElementById("audio");
+	audio.pause();
+	audio.currentTime = 0;
+}
